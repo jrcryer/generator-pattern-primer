108bbeaa771b1c7e2c67eba925b84fadc65ea6b3
James Cryer <info@jamescryer.com>
2014-02-16 04:55:48 +0000

    Adding basic layout

--- app/index.js
+++ app/index.js
@@ -28,27 +28,44 @@ var AppGenerator = module.exports = function Appgenerator(args, options, config)
 util.inherits(AppGenerator, yeoman.generators.Base);
 
 AppGenerator.prototype.askFor = function askFor() {
-    var done = this.async();
+    var cb = this.async();
 
-    // have Yeoman greet the user
+  // welcome message
+  if (!this.options['skip-welcome-message']) {
     console.log(this.yeoman);
+    console.log(chalk.magenta('Out of the box I include HTML5 Boilerplate and a Gruntfile.js to build your app.'));
+  }
 
-    // replace it with a short and sweet description of your generator
-    console.log(chalk.magenta('You\'re using the fantastic PatternPrimer generator.'));
+  var prompts = [{
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Sass with Compass',
+      value: 'includeSass',
+      checked: true
+    }, {
+      name: 'Bootstrap',
+      value: 'includeBootstrap',
+      checked: true
+    }, {
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    }]
+  }];
 
-    // var prompts = [{
-    //   type: 'confirm',
-    //   name: 'someOption',
-    //   message: 'Would you like to enable this option?',
-    //   default: true
-    // }];
+  this.prompt(prompts, function (answers) {
+    var features = answers.features;
 
-    // this.prompt(prompts, function (props) {
-    //   this.someOption = props.someOption;
+    function hasFeature(feat) { return features.indexOf(feat) !== -1; }
 
-    //   done();
-    // }.bind(this));
-    done();
+    this.includeSass = hasFeature('includeSass');
+    this.includeBootstrap = hasFeature('includeBootstrap');
+    this.includeModernizr = hasFeature('includeModernizr');
+
+    cb();
+  }.bind(this));
 };
 
 AppGenerator.prototype.gruntfile = function gruntfile() {
@@ -61,15 +78,58 @@ AppGenerator.prototype.packageJSON = function packageJSON() {
 
 AppGenerator.prototype.bower = function bower() {
   this.copy('bowerrc', '.bowerrc');
-  this.copy('_bower.json', 'bower.json');
+  this.template('_bower.json', 'bower.json');
 };
 
 AppGenerator.prototype.editorConfig = function editorConfig() {
   this.copy('editorconfig', '.editorconfig');
 };
 
+AppGenerator.prototype.h5bp = function h5bp() {
+  this.copy('favicon.ico', 'app/favicon.ico');
+  this.copy('404.html', 'app/404.html');
+  this.copy('robots.txt', 'app/robots.txt');
+  this.copy('htaccess', 'app/.htaccess');
+};
+
 AppGenerator.prototype.jshint = function jshint() {
   this.copy('jshintrc', '.jshintrc');
+};
+
+AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
+  var css = 'main.' + (this.includeSass ? 's' : '') + 'css';
+  this.copy(css, 'app/styles/' + css);
+};
+
+AppGenerator.prototype.writeIndex = function writeIndex() {
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  this.indexFile = this.engine(this.indexFile, this);
+
+  // wire Twitter Bootstrap plugins
+  if (this.includeBootstrap) {
+    var bs = 'bower_components/bootstrap' + (this.includeSass ? '-sass/vendor/assets/javascripts/bootstrap/' : '/js/');
+    this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+      bs + 'affix.js',
+      bs + 'alert.js',
+      bs + 'dropdown.js',
+      bs + 'tooltip.js',
+      bs + 'modal.js',
+      bs + 'transition.js',
+      bs + 'button.js',
+      bs + 'popover.js',
+      bs + 'carousel.js',
+      bs + 'scrollspy.js',
+      bs + 'collapse.js',
+      bs + 'tab.js'
+    ]);
+  }
+
+  this.indexFile = this.appendFiles({
+    html: this.indexFile,
+    fileType: 'js',
+    optimizedPath: 'scripts/main.js',
+    sourceFileList: ['scripts/main.js']
+  });
 };
 
 
@@ -79,7 +139,7 @@ AppGenerator.prototype.app = function app() {
   this.mkdir('app/scripts');
   this.mkdir('app/images');
 
-  this.copy('index.html', 'app/index.html');
+  this.write('app/index.html', this.indexFile);
   this.directory('patterns', 'app/patterns');
 };
 
