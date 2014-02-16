29682aedefeee4503067ab43aa8c42d2ba1f7a15
James Cryer <info@jamescryer.com>
2014-02-16 01:51:57 +0000

    Generator re-structure

--- app/index.js
+++ app/index.js
@@ -5,25 +5,29 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 
-var PatternPrimerGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+var AppGenerator = module.exports = function Appgenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies({
-          bower: false,
-          npm: true,
-          skipInstall: false,
-          callback: function () {
-            console.log('Everything is ready!');
-          }
-        });
-      }
-    });
-  },
+  this.options = options;
 
-  askFor: function () {
+  this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+  this.on('end', function () {
+    if (!this.options['skip-install']) {
+      this.installDependencies({
+        bower: false,
+        npm: true,
+        skipInstall: false,
+        callback: function () {
+          console.log('Everything is ready!');
+        }
+      });
+    }
+  });
+}
+util.inherits(AppGenerator, yeoman.generators.Base);
+
+AppGenerator.prototype.askFor = function askFor() {
     var done = this.async();
 
     // have Yeoman greet the user
@@ -45,25 +49,49 @@ var PatternPrimerGenerator = yeoman.generators.Base.extend({
     //   done();
     // }.bind(this));
     done();
-  },
+};
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/styles');
-    this.mkdir('app/scripts');
+AppGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('_gruntfile.js', 'Gruntfile.js');
+};
 
-    this.template('_package.json', 'package.json');
-    this.template('_bower.json', 'bower.json');
-    this.template('_gruntfile.js', 'Gruntfile.js');
+AppGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
 
-    this.copy('index.html', 'app/index.html');
-    this.directory('patterns', 'app/patterns');
-  },
+AppGenerator.prototype.bower = function bower() {
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('_bower.json', 'bower.json');
+};
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
+AppGenerator.prototype.editorConfig = function editorConfig() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+AppGenerator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+
+AppGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/styles');
+  this.mkdir('app/scripts');
+  this.mkdir('app/images');
+
+  this.copy('index.html', 'app/index.html');
+  this.directory('patterns', 'app/patterns');
+};
+
+AppGenerator.prototype.install = function install() {
+  if (this.options['skip-install']) {
+    return;
   }
-});
 
-module.exports = PatternPrimerGenerator;
+  var done = this.async();
+  this.installDependencies({
+    skipMessage: this.options['skip-install-message'],
+    skipInstall: this.options['skip-install'],
+    callback: done
+  });
+};
